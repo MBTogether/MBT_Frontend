39d4963d4367f69795bb62b79ec4cee47b2923e7
JaeHyun <soehd0889@naver.com>
2022-02-08 22:20:21 +0900

    ⚙️ App.js setting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from "./GlobalStyle";
+import Routing from "./router/UserRouter"
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <GlobalStyle/>
+      <Routing />
+    </BrowserRouter>
   );
 };
 
