14c8bf642d398ae26d68de933e1637e1a32c8ebf
JaeHyun <soehd0889@naver.com>
2022-02-08 22:19:52 +0900

    ⚙️ UserRouter Setting

---
+++ src/router/UserRouter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+const UserRouter = () => {
+    return (
+        <Routes>
+            {/* <Route path="" component={} /> */} {/* 이런 형식으로 component를 import해와서 path의 주소입력하면 그 페이지로 이동됨 */}
+        </Routes>
+    );
+};
+
+export default UserRouter;
