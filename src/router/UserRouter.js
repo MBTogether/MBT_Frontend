5f607c6a12745e5fd0448371ec792156feeeb7bf
JaeHyun <soehd0889@naver.com>
2022-02-09 17:02:19 +0900

    ⚙️ react-router-dom 예시

--- src/router/UserRouter.js
+++ src/router/UserRouter.js
@@ -6,7 +6,7 @@ import Test from '../components/test/Test';
 const UserRouter = () => {
     return (
         <Routes>
-            <Route path="/test" element={<Test/>} />
+            <Route path="/test" element={<Test/>} />  {/* 컴포넌트 이렇게 이용 해주세요 */}
         </Routes>
     );
 };
