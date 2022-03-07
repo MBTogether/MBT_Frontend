6fb21b4bfca82c22d99f7c9156a4d5cd2b5d139f
kimkh05 <nestjs01@naver.com>
2022-03-07 19:56:30 +0900

    📑 메인페이지 퍼블리싱

--- src/router/UserRouter.js
+++ src/router/UserRouter.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import Test from '../components/test/Test';
 import Main from '../components/main/main'
+import Search from '../components/search/search';
+import Mbti from '../components/mbti/mbti';
 
 const UserRouter = () => {
     return (
