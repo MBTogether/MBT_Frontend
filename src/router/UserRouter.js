5d75a03a347065501eb40f31ee398e88a1031f85
kimkh05 <nestjs01@naver.com>
2022-03-07 19:58:17 +0900

    📑 검색페이지 퍼블리싱

--- src/router/UserRouter.js
+++ src/router/UserRouter.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import Test from '../components/test/Test';
+import Search from '../components/search/search';
 
 
 const UserRouter = () => {
     return (
         <Routes>
             <Route path="/test" element={<Test/>} />  {/* 컴포넌트 이렇게 이용 해주세요 */}
+            <Route path="/search" element={<Search />} />
         </Routes>
     );
 };
