2701bc1d4c1f3213ff4b7feec64396309412cd87
JaeHyun <soehd0889@naver.com>
2022-03-08 08:43:18 +0900

    📑 my-page router 설정

--- src/router/UserRouter.js
+++ src/router/UserRouter.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Test from '../components/test/Test';
-
+import Test from "../components/test/Test";
+import MyPage from "../components/myPage/MyPage";
 
 const UserRouter = () => {
-    return (
-        <Routes>
-            <Route path="/test" element={<Test/>} />  {/* 컴포넌트 이렇게 이용 해주세요 */}
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/test" element={<Test />} />{" "}
+      {/* 컴포넌트 이렇게 이용 해주세요 */}
+      <Route path="/my-page" element={<MyPage />} />{" "}
+      {/* 컴포넌트 이렇게 이용 해주세요 */}
+    </Routes>
+  );
 };
 
 export default UserRouter;
