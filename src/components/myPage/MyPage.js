1b5ab4b856024da8543ea7181aafe54dd170a2e5
JaeHyun <soehd0889@naver.com>
2022-03-08 08:43:46 +0900

    ➕ 마이페이지 파일 추가

---
+++ src/components/myPage/MyPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import * as S from "./style";
+import Header from "../common/header/Header";
+
+const MyPage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default MyPage;
