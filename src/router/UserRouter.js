8a626d51793afde8399697b70b185ff7e23b7201
kimkh05 <nestjs01@naver.com>
2022-03-09 20:36:57 +0900

    ❌ 불필요 코드 삭제

--- src/router/UserRouter.js
+++ src/router/UserRouter.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import Test from '../components/test/Test';
 import Main from '../components/main/main'
-import Search from '../components/search/search';
-import Mbti from '../components/mbti/mbti';
 
 const UserRouter = () => {
     return (
