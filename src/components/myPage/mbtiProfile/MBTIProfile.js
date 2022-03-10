52bb514ea017fa50d705df346650637672873d07
JaeHyun <soehd0889@naver.com>
2022-03-10 20:28:26 +0900

    📑 IMAGE import

--- src/components/myPage/mbtiProfile/MBTIProfile.js
+++ src/components/myPage/mbtiProfile/MBTIProfile.js
@@ -1,6 +1,24 @@
 import React from "react";
 import * as S from "./style";
 import { MBTITypeArray } from "../../../constance/myPage/index";
+import {
+  INFP,
+  ENFP,
+  ISFP,
+  ESFP,
+  ENTJ,
+  INTJ,
+  ISTJ,
+  ESTJ,
+  ENFJ,
+  INFJ,
+  ISFJ,
+  ESFJ,
+  ENTP,
+  INTP,
+  ISTP,
+  ESTP,
+} from "../../../assets/index";
 
 const MBTIProfile = (props) => {
   const { mbti } = props;
@@ -10,7 +28,7 @@ const MBTIProfile = (props) => {
         if (mbti === value.mbti)
           return (
             <S.MbtiProfilWrapper key={index}>
-              <img src={value.mbti === "" ? " " : ""} alt="mbtiCharater" />
+              <img src={ISTP} alt="mbtiCharater" />
               <p style={{ color: `${value.color}` }}>{value.mbti}</p>
             </S.MbtiProfilWrapper>
           );
