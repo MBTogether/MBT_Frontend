bc69e24fe0954d9963372e4fd1de9efc27199e8c
JaeHyun <soehd0889@naver.com>
2022-03-11 08:39:05 +0900

    📑 퍼블리싱

---
+++ src/components/myPage/myPageList/ListContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import * as S from "./style";
+
+const ListContainer = () => {
+  const like = "좋아요를 단 게시물";
+  return (
+    <S.MyPageList>
+      <h1>{like}</h1>
+    </S.MyPageList>
+  );
+};
+
+export default ListContainer;
