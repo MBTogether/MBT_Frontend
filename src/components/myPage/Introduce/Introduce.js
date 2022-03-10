be5987797fadf55ddf8b4d27976442755c1a11b0
JaeHyun <soehd0889@naver.com>
2022-03-10 22:28:04 +0900

    📑 마이페이지 소개 퍼블리싱

---
+++ src/components/myPage/Introduce/Introduce.js
@@ -0,0 +1,28 @@
+import React from "react";
+import * as S from "./style";
+import { ModifyIcon } from "../../../assets/index";
+
+const Introduce = () => {
+  return (
+    <S.IntroduceContainer>
+      <S.TitleWrapper>
+        <b>정지원</b>
+        <p>님</p>
+        <img src={ModifyIcon} alt="modifyIcon" />
+      </S.TitleWrapper>
+      <S.IntroduceWrapper>
+        <p>안녕하세요, 반갑습니다. 친하게 지내요</p>
+        <img src={ModifyIcon} alt="modifyIcon" />
+      </S.IntroduceWrapper>
+      <S.SettingBlock>
+        <button className="succesButton button">MBTI 재검사하기</button>
+        <div className="buttonFlexEnd">
+          <button className="succesButton button">비밀번호 변경</button>
+          <button className="secessionButton button">탈퇴하기</button>
+        </div>
+      </S.SettingBlock>
+    </S.IntroduceContainer>
+  );
+};
+
+export default Introduce;
