a46b60b50475485703b9d3a72814550f862f08fa
JaeHyun <soehd0889@naver.com>
2022-03-10 22:28:28 +0900

    📑 mbti에 따른 이미지 나타내기 기능 추가

--- src/components/myPage/mbtiProfile/MBTIProfile.js
+++ src/components/myPage/mbtiProfile/MBTIProfile.js
@@ -1,24 +1,7 @@
+/* eslint-disable array-callback-return */
 import React from "react";
 import * as S from "./style";
 import { MBTITypeArray } from "../../../constance/myPage/index";
-import {
-  INFP,
-  ENFP,
-  ISFP,
-  ESFP,
-  ENTJ,
-  INTJ,
-  ISTJ,
-  ESTJ,
-  ENFJ,
-  INFJ,
-  ISFJ,
-  ESFJ,
-  ENTP,
-  INTP,
-  ISTP,
-  ESTP,
-} from "../../../assets/index";
 
 const MBTIProfile = (props) => {
   const { mbti } = props;
@@ -28,7 +11,10 @@ const MBTIProfile = (props) => {
         if (mbti === value.mbti)
           return (
             <S.MbtiProfilWrapper key={index}>
-              <img src={ISTP} alt="mbtiCharater" />
+              <img
+                src={require(`../../../assets/image/${value.mbti}.svg`)}
+                alt="mbtiCharacter"
+              />
               <p style={{ color: `${value.color}` }}>{value.mbti}</p>
             </S.MbtiProfilWrapper>
           );
