80b1310bb1bc1d1b3d15101bc2b6099435d3f32a
kimkh05 <nestjs01@naver.com>
2022-03-13 23:17:34 +0900

    ➕더미데이터 추가

---
+++ src/constance/mbti/index.js
@@ -0,0 +1,29 @@
+export const SearchList = [
+  { mbti: "ISTJ" },
+  { mbti: "ISFJ" },
+  { mbti: "INFJ" },
+  { mbti: "INTJ" },
+  { mbti: "ISTP" },
+  { mbti: "ISFP" },
+  { mbti: "INFP" },
+  { mbti: "INTP" },
+  { mbti: "ESTP" },
+  { mbti: "ESFP" },
+  { mbti: "ENFP" },
+  { mbti: "ENTP" },
+  { mbti: "ESTJ" },
+  { mbti: "ESFJ" },
+  { mbti: "ENFJ" },
+  { mbti: "ENTJ" },
+];
+export const mbti = "INFP";
+
+export const List = [
+  { title: "안녕하세요.", day: "2020.02.28", img: "1" },
+  { title: "안녕하세요.1", day: "2020.02.28", img: "1" },
+  { title: "안녕하세요.2", day: "2020.02.28", img: "1" },
+  { title: "안녕하세요.3", day: "2020.02.28", img: "1" },
+  { title: "안녕하세요.4", day: "2020.02.28", img: "1" },
+  { title: "안녕하세요.5", day: "2020.02.28", img: "1" },
+  { title: "안녕하세요.6", day: "2020.02.28", img: "1" },
+];
