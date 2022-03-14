776a9955b6a264310b185585e6bec0b6282b379d
kimkh05 <nestjs01@naver.com>
2022-03-14 19:14:10 +0900

    ➕ 검색페이지 더미데이터 추가

---
+++ src/constance/search/index.js
@@ -0,0 +1,52 @@
+export const SearchList = [
+  { mbti: "모든" },
+  { mbti: "ISTJ" },
+  { mbti: "ISFJ" },
+  { mbti: "INFJ" },
+  { mbti: "INTJ" },
+  { mbti: "ISTP" },
+  { mbti: "ISFP" },
+  { mbti: "INFP" },
+  { mbti: "INTP" },
+  { mbti: "ESTP" },
+  { mbti: "ESFP" },
+  { mbti: "ENFP" },
+  { mbti: "ENTP" },
+  { mbti: "ESTJ" },
+  { mbti: "ESFJ" },
+  { mbti: "ENFJ" },
+  { mbti: "ENTJ" },
+];
+
+export const SearchResult = [
+  {
+    img: "img1",
+    title: "제목1",
+    date: "2022.01.01",
+    explanation: "설명설명설명",
+  },
+  {
+    img: "img2",
+    title: "제목2",
+    date: "2022.01.02",
+    explanation: "설명설명설명",
+  },
+  {
+    img: "img3",
+    title: "제목3",
+    date: "2022.01.03",
+    explanation: "설명설명설명",
+  },
+  {
+    img: "img4",
+    title: "제목4",
+    date: "2022.01.04",
+    explanation: "설명설명설명",
+  },
+  {
+    img: "img5",
+    title: "제목5",
+    date: "2022.01.05",
+    explanation: "설명설명설명",
+  },
+];
