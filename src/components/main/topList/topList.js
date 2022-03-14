4fb75893ca8c12451206e25c0e053d1dad52e695
kimkh05 <nestjs01@naver.com>
2022-03-14 17:04:55 +0900

    📑메인페이지 상단리스트 코드 수정

--- src/components/main/topList/topList.js
+++ src/components/main/topList/topList.js
@@ -10,21 +10,19 @@ const TopList = () => {
       <S.LeftArrow direction="prev">&lt;</S.LeftArrow>
       {MainMBTIData.map((value, listPage) => {
         if (listPage < 7) {
-          if (value.mbti === listMbti) {
-            return (
-              <div className="mbtiList" key={listPage}>
-                <S.ListTop>
-                  <span className="mbti">{value.mbti}</span>
-                  <span className="characteristics">
-                    : {value.characteristic}
-                  </span>
-                </S.ListTop>
-                <S.ListBottom>
-                  <span className="introduce">{value.introduce}</span>
-                </S.ListBottom>
-              </div>
-            );
-          }
+          return (
+            <div className="mbtiList" key={listPage}>
+              <S.ListTop>
+                <span className="mbti">{value.mbti}</span>
+                <span className="characteristics">
+                  : {value.characteristic}
+                </span>
+              </S.ListTop>
+              <S.ListBottom>
+                <span className="introduce">{value.introduce}</span>
+              </S.ListBottom>
+            </div>
+          );
         }
       })}
       <S.RightArrow direction="next">&gt;</S.RightArrow>
