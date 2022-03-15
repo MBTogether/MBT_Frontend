5e7d26ab5b6c568b873d14462d72307ad6b5b40c
kimkh05 <nestjs01@naver.com>
2022-03-15 21:38:25 +0900

    ❌불필요 코드 삭제

--- src/constance/main/index.js
+++ src/constance/main/index.js
@@ -1,118 +1,83 @@
-import {
-  ISTJ,
-  ISFJ,
-  INFJ,
-  INTJ,
-  ISTP,
-  ISFP,
-  INFP,
-  INTP,
-  ESTP,
-  ESFP,
-  ENFP,
-  ENTP,
-  ESTJ,
-  ESFJ,
-  ENFJ,
-  ENTJ,
-} from "../.././assets/index";
-
 export const MainMBTIData = [
   {
     mbti: "ISTJ",
     characteristic: "청렴결백한 논리주의자",
     introduce: "한번 시작한 일은 끝까지 해내는 사람",
-    img: { ISTJ },
   },
   {
     mbti: "ISFJ",
     characteristic: "용감한 수호자",
     introduce: "성실하고 온화하며 협조를 잘하는 사람",
-    img: { ISFJ },
   },
   {
     mbti: "INFJ",
     characteristic: "선의의 옹호자",
     introduce: "사람과 관련된 뛰어난 통찰력을 가지고 있는 사람",
-    img: { INFJ },
   },
   {
     mbti: "INTJ",
     characteristic: "용의주도한 전략가",
     introduce: "직관력이 뛰어나며 철두철미한 계획을 세우는 전략가",
-    img: { INTJ },
   },
   {
     mbti: "ISTP",
     characteristic: "만능 재능꾼",
     introduce: "논리적으로 뛰어난 상황 적응력을 가지고 있는 사람",
-    img: { ISTP },
   },
   {
     mbti: "ISFP",
     characteristic: "호기심 많은 예술가",
     introduce: "따뜻한 감정을 가진 융통성 있는 예술가",
-    img: { ISFP },
   },
   {
     mbti: "INFP",
     characteristic: "열정적인 중재자",
     introduce: "이상적인 세상을 만들어 가는 사람",
-    img: { INFP },
   },
   {
     mbti: "INTP",
     characteristic: "논리적인 사색가",
     introduce: "끊임없이 지식에 목말라하는 사색가",
-    img: { INTP },
   },
   {
     mbti: "ESTP",
     characteristic: "모혐을 즐기는 사업가",
     introduce: "친구, 운동, 음식 등 다양한 활동을 선호하는 사람",
-    img: { ESTP },
   },
   {
     mbti: "ESFP",
     characteristic: "자유로운 영혼의 연예인",
     introduce: "분위기를 고조시키는 우호적 사람",
-    img: { ESFP },
   },
   {
     mbti: "ENFP",
     characteristic: "재기발랄한 활동가",
     introduce: "열정적으로 새로운 관계를 만드는 사람",
-    img: { ENFP },
   },
   {
     mbti: "ENTP",
     characteristic: "뜨거운 논쟁을 즐기는 변론가",
     introduce: "풍부한 상상력을 가지고 새로운 것에 도전하는 사람",
-    img: { ENTP },
   },
   {
     mbti: "ESTJ",
     characteristic: "엄격한 관리자",
     introduce: "사무적, 실용적, 현실적으로 일을 많이하는 사람",
-    img: { ESTJ },
   },
   {
     mbti: "ESFJ",
     characteristic: "사교적인 외교관",
     introduce: "친절과 현실감을 바탕으로 타인에게 봉사하는 사람",
-    img: { ESFJ },
   },
   {
     mbti: "ENFJ",
     characteristic: "정의로운 사회운동가",
     introduce: "타인의 성장을 도모하고 협동하는 사람",
-    img: { ENFJ },
   },
   {
     mbti: "ENTJ",
     characteristic: "대당한 통솔자",
     introduce: "비전을 가지고 사람들을 활력적으로 이끌어가는 사람",
-    img: { ENTJ },
   },
 ]; // dummy data
 
