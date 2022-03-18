be5e5427dea0cc48c7b8ed37ad39e18b29c9123d
SxxHyun <shp35079@naver.com>
2022-03-18 14:39:29 +0900

    ➕ admin 페이지 라우팅 설정

--- src/router/UserRouter.js
+++ src/router/UserRouter.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Test from '../components/test/Test';
-
+import Test from "../components/test/Test";
+import Admin from "../components/admin/Admin";
 
 const UserRouter = () => {
-    return (
-        <Routes>
-            <Route path="/test" element={<Test/>} />  {/* 컴포넌트 이렇게 이용 해주세요 */}
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/test" element={<Test />} />
+      <Route path="/admin" element={<Admin />} />
+      {/* 컴포넌트 이렇게 이용 해주세요 */}
+    </Routes>
+  );
 };
 
 export default UserRouter;
