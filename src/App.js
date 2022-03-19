f7b247cce621fb2254bfa5c3834f79d2d3a3682b
kimkh05 <nestjs01@naver.com>
2022-03-19 18:59:42 +0900

    📑헤더 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from "./styles/GlobalStyle";
 import UserRouter from './router/UserRouter';
+import Header from './components/common/header/Header';
 
 const App = () => {
   return (
     <BrowserRouter>
       <GlobalStyle/>
+      <Header />
       <UserRouter />
     </BrowserRouter>
   );
