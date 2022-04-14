fe0f1a11a4857f0f0d4830f2bd118ac7193ca048
kimkh05 <nestjs01@naver.com>
2022-04-14 17:07:41 +0900

    ➕ :: Add router Login Page

--- src/router/UserRouter.js
+++ src/router/UserRouter.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Test from '../components/test/Test';
-
+import Login from "../components/loginPage/login";
+import Test from "../components/test/Test";
 
 const UserRouter = () => {
-    return (
-        <Routes>
-            <Route path="/test" element={<Test/>} />  {/* 컴포넌트 이렇게 이용 해주세요 */}
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/test" element={<Test />} />{" "}
+      {/* 컴포넌트 이렇게 이용 해주세요 */}
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 };
 
 export default UserRouter;
